ccfb5011477b81a3596fed5080d94b99f9eba979
Carla Vargas <carlav02@gmail.com>
2024-05-08 16:08:09 -0400

    funciones

--- tiposdatos.py
+++ tiposdatos.py
@@ -2,3 +2,11 @@
 institucion = "Universidad de los Lagos"
 asignatura = 'programacion'
 descripcion = """Asignatura de primer semestre de la carrera de Ing. Civil en Informatica"""
+
+print(institucion[0]) #imprimir la posicion de un caracter, si colocamos un numero en negativo imprime de derecha a izquierda
+ 
+#concatenacion 
+resultado = print(institucion + " " + asignatura )
+salida = print( institucion  * 4 ) #puede multiplicar la variable 
+print(institucion.split()) #funcion split en strings 
+print(len(institucion)) #funcion que cuenta la cantidad de carcateres en la variable   
