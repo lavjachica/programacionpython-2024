6c162dee3c19b769e0aa5b17f536699484538d2c
Carla Vargas <carlav02@gmail.com>
2024-05-08 16:39:46 -0400

    reto terminado

---
+++ Retos/Reto1.py
@@ -0,0 +1,5 @@
+nombre = input("¿Cual es tu nombre?") 
+edad = input("¿Cual es tu edad?")
+total_edad =(int(edad) + 20)
+
+print(f"Hola mi nombre es {nombre}, tengo {edad} años y en 20 años tendre {total_edad}")
