17a578c11e0f3e2e8f268402e5b29913ba9bd6c6
Carla Vargas <carlav02@gmail.com>
2024-06-14 10:01:38 -0400

    actualizacion materia

--- operadores.py
+++ operadores.py
@@ -3,6 +3,7 @@ print("\n################## OPERADORES LOGICOS #####################")
 encendido= True 
 edad= 19
 bencina = True
+
 print("\n #### VALOR AND ####")
 if encendido and bencina: 
     print("el vehiculo puede avanzar. ")
@@ -15,7 +16,7 @@ if bencina or encendido:
     print("el vehiculo puede avanzar .")
 else:
     print("el vehiculo no puede arrancar")
-    
+
 ##DECLARANDO VARIABLES
 a=20
 b=3
@@ -33,3 +34,31 @@ print(animal_domestico > animal_salvaje)
 print(animal_domestico < animal_salvaje) 
 print(animal_domestico >= animal_salvaje)
 print(animal_domestico <= animal_salvaje)
+
+("\n ############## BUCLE, CICLOS, LOOPS #######")
+##### WHILE ITERACIONES NO DEFINIDAS (MIENTRAS EN PSEINT)
+#while true: 
+    #print(num)
+    #num+=2            ###while infinito control C PARA INTERRUMPIR UN BUCLE INFINITO 
+num= 0 
+while num <= 100: 
+    print(num)
+    num += 2
+
+while num <= 100: 
+    print(num)
+    num+= 2
+else: 
+    print("mi condicional es mayor o igual a 100")
+
+####### uso del break 
+while True: 
+    parametro = input(">")
+    if parametro == "exit":
+        break
+    else: 
+        print(parametro)
+            
+
+
+
