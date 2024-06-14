54f0912cc515c47360a6d1e8f732c6b5d4cc2b84
Carla Vargas <carlav02@gmail.com>
2024-06-14 17:49:41 -0400

    actualizacion

--- bucles.py
+++ bucles.py
@@ -15,10 +15,43 @@ else:
     print("mi condicional es mayor o igual a 100")
 
 ####### uso del break 
-while True: 
+"""while True: 
     parametro = input(">")
     if parametro == "exit":
         break
     else: 
-        print(parametro)
+        print(parametro)"""
+####Bucles
+from colorama import init, Fore
+print(Fore.GREEN + "Inicio de ciclo N1")
+while num<= 200: 
+    print(num)
+    num = num + 2
+    #num+=
+print(Fore.YELLOW + "Inicio de Ciclo N2")
+while num <= 200: 
+    print(num)
+    num = num + 2
+else: 
+    print(Fore.RED + "mi condicion es igual o mayor a 200")
+print(Fore.YELLOW + "Cierre de ciclo N2")
+
+print(Fore.CYAN + "Inicio de Ciclo N3")
+while num <= 300:
+    print(num)
+    num = num + 2
+    if num ==250: 
+        print(Fore.YELLOW + "se detiene la ejecucion")
+        break
+
+print(Fore.CYAN + "Cierre de Ciclo N3")
+
+
+print(Fore.LIGHTMAGENTA_EX + "Inicio de Ciclo N4")
+newlista= [1,2,3,4,5,6,7,8,9,10]
+for i in newlista:
+    print(i)
+print(Fore.LIGHTRED_EX + "")
+for i in range(1,11): 
+    print(i)
             
