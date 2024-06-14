34546d5152bef8a604209cbe40eadc75e716f4a1
Carla Vargas <carlav02@gmail.com>
2024-06-14 09:21:51 -0400

    operadores

---
+++ operadores.py
@@ -0,0 +1,35 @@
+
+print("\n################## OPERADORES LOGICOS #####################")
+encendido= True 
+edad= 19
+bencina = True
+print("\n #### VALOR AND ####")
+if encendido and bencina: 
+    print("el vehiculo puede avanzar. ")
+else: 
+    print("el vehiculo no puede arrancar")
+
+####OPERADOR OR
+print("\n #### VALOR OR ####")
+if bencina or encendido:
+    print("el vehiculo puede avanzar .")
+else:
+    print("el vehiculo no puede arrancar")
+    
+##DECLARANDO VARIABLES
+a=20
+b=3
+c= 15
+d= 6
+print("\n################## OPERADORES ARITMETICOS ##################")
+
+print("hola" * (int((10*2) / 5)), "\n")
+print("\n### OPERADORES DE COMPARACION EN STRINGS #########")
+animal_domestico = "gato"
+animal_salvaje = "pato"
+print(animal_domestico == animal_salvaje)
+print(animal_domestico != animal_salvaje)
+print(animal_domestico > animal_salvaje)
+print(animal_domestico < animal_salvaje) 
+print(animal_domestico >= animal_salvaje)
+print(animal_domestico <= animal_salvaje)
