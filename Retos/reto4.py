e22c63218a6ff638506500368bef83f89453db2f
Carla Vargas <carlav02@gmail.com>
2024-06-17 11:24:05 -0400

    reto 4 terminado

---
+++ Retos/reto4.py
@@ -0,0 +1,16 @@
+
+total = 0
+contador = 10
+
+num= 10
+while num >=10 and num <=50: 
+    print(num)
+    num += 2
+
+
+while contador >=10 and contador <=50:
+    total += contador
+    contador += 2
+
+
+print(f"La suma total es {total}")
